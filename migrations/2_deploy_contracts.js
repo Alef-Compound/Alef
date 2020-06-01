93c2d1c26929a6ecb4e1574bf970f14f7c2b89c2
gabkk <gab.kuma@gmail.com>
2020-06-01 22:02:39 +0800

    Fix typo in the Alef base contract, add deployment file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Alef = artifacts.require("./Alef.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Alef);
 };
